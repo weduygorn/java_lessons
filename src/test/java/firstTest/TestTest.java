61c48ca2be96c03a6f2574d54a0ccac115a9701b
sophus <3662944@gmail.com>
2019-02-24 13:26:52 +0200

    newtest

---
+++ src/test/java/firstTest/TestTest.java
@@ -0,0 +1,18 @@
+package firstTest;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import static org.testng.AssertJUnit.assertEquals;
+
+public class TestTest {
+
+    @Test
+    public void testTest() {
+        int a =5;
+        assertEquals(a, 4);
+
+    }
+
+}
+
