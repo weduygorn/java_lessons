7b0e72f4910d436d823e729a7985b9fe62be0241
sophus <3662944@gmail.com>
2019-03-18 23:34:50 +0200

    refactor

--- src/test/java/jkh1557/tests/HomePageTests.java
+++ src/test/java/jkh1557/tests/HomePageTests.java
@@ -11,15 +11,8 @@ public class HomePageTests extends ParentTest {
 
     @Test
     public void testAlliSFineWithHome() throws Exception {
-        String text = app.driver.findElement(By.xpath("//div[@class='claim-message-item']"))
-                .getText();
-
-        assert text
-                .contains("відсутні аварійні або планові відключення");
-
-        System.out.println(text);
-
-
+        // сюда вытянуть метод generalInfo;
+        HomePage hp = new HomePage();
 
 
     }
